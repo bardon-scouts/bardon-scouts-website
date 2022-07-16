97ed1fce1f06f7ae325dc49a3bc40eafc3663380
hamish Currie <hamish@currie.com.au>
2022-07-17 09:14:52 +1000

    hero image working on thanks page.

--- src/pages/thanks/index.md
+++ src/pages/thanks/index.md
@@ -2,6 +2,7 @@
 templateKey: thanks-page
 path: /thanks
 title: Thanks
+subheading: 
 image: /img/buddha-s-light-cubs-bush-walk-2.jpg
 ---
 
--- src/templates/thanks-page.js
+++ src/templates/thanks-page.js
@@ -3,14 +3,20 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import FullWidthImage from "../components/FullWidthImage";
+import { getImage } from "gatsby-plugin-image";
+import logo from "../img/scouts_logo.jpg";
 
 // eslint-disable-next-line
-export const ThanksTemplate = ({ title, content, contentComponent }) => {
+export const ThanksTemplate = ({ title, image,subheading, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
-
+  const heroImage = getImage(image) || image;
+  //const title = post.frontmatter.title;
   return (
 
     <section className="section">
+      <div className="scouts-logo"><img src={logo} alt="Scouts Australia" /></div>
+      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
       <div className="container">
         <div className="content">
           <h1>{title}</h1>
@@ -26,26 +32,30 @@ export const ThanksTemplate = ({ title, content, contentComponent }) => {
 
 ThanksTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  subheading: PropTypes.string,
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 };
 
-const Thanks = ({ data }) => {
+const Thanks = ({ image,data }) => {
   const { markdownRemark: post } = data;
-
   return (
     <Layout>
+
       <ThanksTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        image={post.frontmatter.image}
+        subheading={post.frontmatter.subheading}
       />
     </Layout>
   );
 };
 
 Thanks.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default Thanks;
@@ -56,6 +66,7 @@ export const thanksQuery = graphql`
       html
       frontmatter {
         title
+        subheading
         image {
           childImageSharp {
             gatsbyImageData(quality: 100, layout: FULL_WIDTH)
